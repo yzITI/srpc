a8acd550d8a45cb8b7a95873eac57b543357c134
Phantomlsh <lishiheng2001@163.com>
2023-05-11 10:06:34 -0700

    Add client.js

---
+++ client.js
@@ -0,0 +1,18 @@
+let url = '/'
+
+const getFunction = name => ((...args) => fetch(url, {
+  method: 'POST', mode: 'cors', cache: 'no-cache',
+  headers: { 'Content-Type': 'application/json' },
+  body: JSON.stringify({ N: name, A: args })
+}).then(async r => {
+  if (r.status === 200) return (await r.json()).R
+  else throw (await r.text())
+}))
+
+const proxyGet = (target, prop) => {
+  const key = target.key, newKey = key ? key + '.' + prop : prop, f = getFunction(newKey)
+  f.key = newKey
+  return new Proxy(f, { get: proxyGet })
+}
+
+window.srpc = new Proxy((endpoint = '/') => url = endpoint, { get: proxyGet })
